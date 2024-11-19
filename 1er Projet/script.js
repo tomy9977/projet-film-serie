8fddacdeb2060b0dea7253f5ca7e0e160c7e91b1
tomy9977 <tomprn77@gmail.com>
2024-11-19 12:40:49 +0100

    Add files via upload

---
+++ 1er Projet/script.js
@@ -0,0 +1,158 @@
+//gestion du menu latéral
+let openBtn = document.getElementById("nav-open");
+let closeBtn = document.getElementById("nav-close");
+let navWrapper = document.getElementById("nav-wrapper");
+let navLatteral = document.getElementById("nav-latteral");
+
+const openNav = () => {
+    navWrapper.classList.add("active");
+    navLatteral.style.left = "0";
+}
+
+const closeNav = () => {
+    navWrapper.classList.remove("active");
+    navLatteral.style.left = "-100%";
+}
+
+openBtn .addEventListener("click" , openNav)
+closeBtn .addEventListener("click" , closeNav)
+navWrapper .addEventListener("click" , closeNav)
+
+
+
+//clé api c18a944fa153e0a4327a36d2b9ea020d
+// création du l'url genre
+const baseUrl = `https://api.themoviedb.org/3`;
+const genreUrl = `/genre/movie/list?language=fr`;
+
+const genreLien = `${baseUrl}${genreUrl}`;
+
+const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMThhOTQ0ZmExNTNlMGE0MzI3YTM2ZDJiOWVhMDIwZCIsIm5iZiI6MTcyOTg5MDUzNy4wMDg5MDYsInN1YiI6IjY3MTdkNWEzYTc3YWYxZGJiYWY4YmEzOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xvjyNliU2iApKKlX17SsPrHqlmBx_VXd8B9i9uGOhiA'
+    }
+  };
+
+
+
+async function afficherGenre() {
+
+    const response = await fetch(genreLien, options);
+    const genre = await response.json();
+    const genres = genre.genres;
+    
+
+    for (let i = 0; i < genres.length; i++) {
+    
+        const sectionCategories = document.querySelector(".categories");
+        const categoriesElement = document.createElement("button");
+        categoriesElement.classList.add("genreButton");
+        categoriesElement.id = genres[i].id;
+        categoriesElement.innerText = genres[i].name;
+        categoriesElement.addEventListener("click", (event)=> {
+            afficherFilmsParGenre(event.target.id);
+        })
+        
+        sectionCategories.appendChild(categoriesElement);
+    
+    }
+    
+}
+
+afficherGenre();
+
+
+
+// création de l'URL films du moment
+const NowUrl = `/movie/now_playing?language=fr`
+
+const NowLien = `${baseUrl}${NowUrl}`;
+
+
+
+async function afficherImage (size, imgUrl) {
+    const response = await fetch(`https://image.tmdb.org/t/p/${size}${imgUrl}?api_key=c18a944fa153e0a4327a36d2b9ea020d`);
+    return response;
+} 
+
+
+
+async function afficherFilmsDuMoment() {
+    const response = await fetch(NowLien, options);
+    const now = await response.json();
+    const nowPlaying = now.results;
+    
+
+    for (let i = 0; i < nowPlaying.length; i++) {
+
+        const sectionAffiches = document.querySelector(".affiches");
+        const affichesElement = document.createElement("img");
+        affichesElement.src = (await afficherImage("w200", nowPlaying[i].poster_path)).url;
+
+        sectionAffiches.appendChild(affichesElement);
+    }
+}
+
+afficherFilmsDuMoment();
+
+
+
+// Gestion du bouton "Voir plus de film"
+
+let page = 1;
+
+async function afficherPage (page) {
+
+    const response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?language=fr&page=${page}`, options);
+    const now = await response.json();
+    const nowPlaying = now.results;
+    
+
+    for (let i = 0; i < nowPlaying.length; i++) {
+
+        const sectionAffiches = document.querySelector(".affiches");
+        const affichesElement = document.createElement("img");
+        affichesElement.src = (await afficherImage("w200", nowPlaying[i].poster_path)).url;
+
+        sectionAffiches.appendChild(affichesElement);
+    }
+}
+
+const button = document.getElementById("voirPlusDeFilms");
+button.addEventListener("click", async () => {
+    page++;
+    afficherPage(page);
+})
+
+
+
+
+// afficher les films selon les genres
+async function afficherFilmsParGenre(genreId) {
+    const response = await fetch(NowLien, options);
+    const now = await response.json();
+    const nowPlaying = now.results;
+    
+
+
+    for (let i = 0; i < nowPlaying.length; i++) {
+
+        const sectionAffiches = document.querySelector(".affiches");
+        const affichesElement = document.createElement("img");
+        affichesElement.src = (await afficherImage("w200", nowPlaying[i].poster_path)).url;
+
+        sectionAffiches.appendChild(affichesElement);
+    }
+}
+
+
+// passer de films à series
+const boutonSeries = document.getElementById("series");
+boutonSeries.addEventListener("click", ()=> {
+    document.location.href="series.html";
+})
+
+// au click sur le film
+
---
+++ 1er Projet/series.js
@@ -0,0 +1,131 @@
+//gestion du menu latéral
+let openBtn = document.getElementById("nav-open");
+let closeBtn = document.getElementById("nav-close");
+let navWrapper = document.getElementById("nav-wrapper");
+let navLatteral = document.getElementById("nav-latteral");
+
+const openNav = () => {
+    navWrapper.classList.add("active");
+    navLatteral.style.left = "0";
+}
+
+const closeNav = () => {
+    navWrapper.classList.remove("active");
+    navLatteral.style.left = "-100%";
+}
+
+openBtn .addEventListener("click" , openNav)
+closeBtn .addEventListener("click" , closeNav)
+navWrapper .addEventListener("click" , closeNav)
+
+
+
+//clé api c18a944fa153e0a4327a36d2b9ea020d
+// création du l'url genre
+const baseUrl = `https://api.themoviedb.org/3`;
+const genreUrl = `/genre/movie/list?language=fr`;
+
+const genreLien = `${baseUrl}${genreUrl}`;
+
+const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMThhOTQ0ZmExNTNlMGE0MzI3YTM2ZDJiOWVhMDIwZCIsIm5iZiI6MTcyOTg5MDUzNy4wMDg5MDYsInN1YiI6IjY3MTdkNWEzYTc3YWYxZGJiYWY4YmEzOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xvjyNliU2iApKKlX17SsPrHqlmBx_VXd8B9i9uGOhiA'
+    }
+  };
+
+
+
+async function afficherGenre() {
+
+    const response = await fetch(genreLien, options);
+    const genre = await response.json();
+    const genres = genre.genres;
+    
+
+    for (let i = 0; i < genres.length; i++) {
+    
+        const sectionCategories = document.querySelector(".categories");
+        const categoriesElement = document.createElement("a");
+        categoriesElement.innerText = genres[i].name;
+        
+        
+        sectionCategories.appendChild(categoriesElement);
+    
+    }
+    
+}
+
+afficherGenre();
+
+
+
+async function afficherImage (size, imgUrl) {
+    const response = await fetch(`https://image.tmdb.org/t/p/${size}${imgUrl}?api_key=c18a944fa153e0a4327a36d2b9ea020d`);
+    return response;
+} 
+
+
+
+// afficher les séries du moment
+const serieUrl = `/tv/airing_today?language=fr`
+
+const serieLien = `${baseUrl}${serieUrl}`
+
+async function afficherSeries () {
+    const response = await fetch(serieLien, options);
+    const serie = await response.json();
+    const series = serie.results;
+
+
+    for (let i = 0; i < series.length; i++) {
+
+       
+            const sectionAffiches = document.querySelector(".affiches");
+            const affichesElement = document.createElement("img");
+            affichesElement.src = (await afficherImage("w200", series[i].poster_path)).url;
+
+            sectionAffiches.appendChild(affichesElement);
+           
+    }
+    
+}
+afficherSeries();
+
+
+
+// Gestion du bouton "Voir plus de series"
+
+let page = 1;
+
+async function afficherPage (page) {
+
+    const response = await fetch(`https://api.themoviedb.org/3/tv/airing_today?language=fr&page=${page}`, options);
+    const now = await response.json();
+    const nowPlaying = now.results;
+    
+
+    for (let i = 0; i < nowPlaying.length; i++) {
+
+        const sectionAffiches = document.querySelector(".affiches");
+        const affichesElement = document.createElement("img");
+        affichesElement.src = (await afficherImage("w200", nowPlaying[i].poster_path)).url;
+
+        sectionAffiches.appendChild(affichesElement);
+    }
+}
+
+const button = document.getElementById("voirPlusDeSeries");
+button.addEventListener("click", async () => {
+    page++;
+    afficherPage(page);
+})
+
+
+
+// passer de series à films
+const boutonSeries = document.getElementById("films");
+boutonSeries.addEventListener("click", ()=> {
+    document.location.href="index.html";
+})
